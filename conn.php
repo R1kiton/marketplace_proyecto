13ce56b3de0e2a90622d3b7efaeb45f12ffe3d01
Luis Francisco Gudiño Garcia <luis.gudino@iest.edu.mx>
2026-09-09 23:24:52 -0600

    conexion a base real

--- conn.php
+++ conn.php
@@ -1,7 +1,20 @@
 <?php
+$host     = "mysql-xxxx-tu-proyecto.aivencloud.com"; 
+$port     = "12345";                                 
+$dbname   = "defaultdb";                             
+$username = "avnadmin";                             
+$password = "tu_contraseña_aiven";                   
+$ssl_ca   = __DIR__ . "/ca.pem";                     
 
-$conn = new PDO("mysql:host=localhost;dbname=mercado_libre;charset=utf8","root","");
+try {
+    $options = [
+        PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ];
 
-
-
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password, $options);
+    
+} catch (PDOException $e) {
+    die("Error al conectar: " . $e->getMessage());
+}
 ?>
