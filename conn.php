019a18bf6dfd06d0e83d31b15c33b2e259f18f5f
Luis Francisco Gudiño Garcia <luis.gudino@iest.edu.mx>
2026-09-09 23:38:44 -0600

    conexion base de datos

--- conn.php
+++ conn.php
@@ -1,4 +1,10 @@
 <?php
+$host     = "mysql-1a314ecb-mercado-fake.b.aivencloud.com"; 
+$port     = "14161";                                 
+$dbname   = "defaultdb";                             
+$username = "avnadmin";                             
+$password = "AVNS_UFuayGXIvzz9FG00QIk";                   
+$ssl_ca   = __DIR__ . "/ca.pem";                     
 
 $conn = new PDO("mysql:host=localhost;dbname=mercado_libre;charset=utf8","root","");
 
